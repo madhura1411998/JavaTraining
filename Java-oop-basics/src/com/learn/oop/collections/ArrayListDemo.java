f5ccce7836d278d8ba54b8036de0550a3f1fc1df
Madhura <mkokate1@gmail.com>
2022-11-08 18:49:12 +0530

    time collection arraylist

---
+++ Java-oop-basics/src/com/learn/oop/collections/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.learn.oop.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> participantList=new ArrayList<>();
+		participantList.add("Madhura");
+		participantList.add("Riya");
+		participantList.add("Ram");
+		participantList.add("Sita");
+		participantList.add("Sam");
+		System.out.println(participantList);
+		
+	}
+}
--- Java-oop-basics/src/com/learn/oop/date/DateDemo.java
+++ Java-oop-basics/src/com/learn/oop/date/DateDemo.java
@@ -1,14 +1,18 @@
 package com.learn.oop.date;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DateDemo {
 	public static void main(String[] args) {
 		LocalDate date=LocalDate.now();
 		LocalDate yesterday=date.minusDays(1);
 		LocalDate tommorow=date.plusDays(1);
+		LocalDateTime time=LocalDateTime.now();
+
 		System.out.println("Date : "+date);
 		System.out.println("Yesterday's date is : "+yesterday);
 		System.out.println("Tommorow's date is : "+tommorow);
+		System.out.println("Time : "+time);
 	}
 }

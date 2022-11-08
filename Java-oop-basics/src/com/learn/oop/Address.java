419bb5862e4a33a78b6221f4ddd3fdcf312d1ff3
Madhura <mkokate1@gmail.com>
2022-11-08 16:30:45 +0530

    aggregate

---
+++ Java-oop-basics/src/com/learn/oop/Address.java
@@ -0,0 +1,15 @@
+package com.learn.oop;
+
+public class Address {
+
+	String state;
+	String city;
+	String country;
+	public Address(String state, String city, String country) {
+		this.state = state;
+		this.city = city;
+		this.country = country;
+	}
+	
+	
+}

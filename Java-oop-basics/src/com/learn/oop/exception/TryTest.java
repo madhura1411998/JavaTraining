5904c49a4c4cc06927b748018e214d820aeaec3a
Madhura <mkokate1@gmail.com>
2022-11-08 17:53:43 +0530

    exception handling try catch throws

---
+++ Java-oop-basics/src/com/learn/oop/exception/TryTest.java
@@ -0,0 +1,14 @@
+package com.learn.oop.exception;
+
+public class TryTest {
+	public static void main(String[] args) {
+		try {
+			Integer num=100/0;
+		}
+		catch(ArithmeticException e) {
+			System.out.println(e);
+		}
+		System.out.println("Continue");
+	}
+	
+}
